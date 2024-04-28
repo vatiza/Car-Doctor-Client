db8d1ff2d774cdd078fdfb0b4d31f26d1a0f8229
vatiza <rakibur.mac@gmail.com>
2024-04-28 13:12:01 +0600

    carousel done

--- src/Componets/Pages/Home/Banner/Banner.jsx
+++ src/Componets/Pages/Home/Banner/Banner.jsx
@@ -7,8 +7,8 @@ const Banner = () => {
   return (
     <div className="carousel w-full rounded">
       <div id="slide1" className="carousel-item relative w-full">
-        <img src={img1} className="w-full" />
-        <div className="absolute flex h-full items-center left-0 top-0 bg-gradient-to-r from-[#151515] bottom-0">
+        <img src={img1} className="w-full rounded-xl" />
+        <div className="absolute rounded-xl flex h-full items-center left-0 top-0 bg-gradient-to-r from-[#151515] bottom-0">
           <div className="text-white space-y-7 w-1/2 pl-8">
             <h1 className="text-6xl font-bold">
               Affordable Price For Car Servicing
@@ -31,7 +31,20 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
-        <img src={img2} className="w-full" />
+        <img src={img2} className="w-full rounded-xl" />
+        <div className="absolute rounded-xl flex h-full items-center left-0 top-0 bg-gradient-to-r from-[#151515] bottom-0">
+          <div className="text-white space-y-7 w-1/2 pl-8">
+            <h1 className="text-6xl font-bold">
+              Affordable Price For Car Servicing
+            </h1>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <button className="btn btn-warning me-5">Discover More</button>
+            <button className="btn btn-outline">Latest Project</button>
+          </div>
+        </div>
         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide1" className="btn btn-circle me-5">
             ❮
@@ -42,7 +55,20 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide3" className="carousel-item relative w-full">
-        <img src={img3} className="w-full" />
+        <img src={img3} className="w-full rounded-xl" />
+        <div className="absolute rounded-xl flex h-full items-center left-0 top-0 bg-gradient-to-r from-[#151515] bottom-0">
+          <div className="text-white space-y-7 w-1/2 pl-8">
+            <h1 className="text-6xl font-bold">
+              Affordable Price For Car Servicing
+            </h1>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <button className="btn btn-warning me-5">Discover More</button>
+            <button className="btn btn-outline">Latest Project</button>
+          </div>
+        </div>
         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide2" className="btn btn-circle me-5">
             ❮
@@ -53,7 +79,20 @@ const Banner = () => {
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full">
-        <img src={img4} className="w-full" />
+        <img src={img4} className="w-full rounded-xl" />
+        <div className="absolute rounded-xl flex h-full items-center left-0 top-0 bg-gradient-to-r from-[#151515] bottom-0">
+          <div className="text-white space-y-7 w-1/2 pl-8">
+            <h1 className="text-6xl font-bold">
+              Affordable Price For Car Servicing
+            </h1>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <button className="btn btn-warning me-5">Discover More</button>
+            <button className="btn btn-outline">Latest Project</button>
+          </div>
+        </div>
         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
           <a href="#slide3" className="btn btn-circle me-5">
             ❮
