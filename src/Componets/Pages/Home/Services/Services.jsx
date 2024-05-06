e052f12d11b9cb5e61b6ad7bc2cf9881bbef2ad1
vatiza <rakibur.mac@gmail.com>
2024-05-06 21:15:51 +0600

    search field

--- src/Componets/Pages/Home/Services/Services.jsx
+++ src/Componets/Pages/Home/Services/Services.jsx
@@ -1,13 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServicesCards from "./ServicesCards";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [asc, setAsc] = useState(true);
+  const [search, setSearch] = useState("");
+  const searchRef = useRef(null);
+
+  const handleSearch = () => {
+    setSearch(searchRef.current.value);
+  };
+
   useEffect(() => {
-    fetch("https://car-doctor-js-server.vercel.app/services")
+    fetch(
+      `http://localhost:5000/services?search=${search}&sort=${
+        asc ? "asc" : "desc"
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [asc, search]);
 
   return (
     <div className="mt-5 mb-5">
@@ -19,6 +31,38 @@ const Services = () => {
           humour, or randomised <br /> words which don't look even slightly
           believable.{" "}
         </p>
+        <div className="form-control">
+          <div className="input-group">
+            <input
+              ref={searchRef}
+              type="text"
+              placeholder="Search…"
+              className="input input-bordered"
+            />
+            <button onClick={handleSearch} className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <button
+          onClick={() => setAsc(!asc)}
+          className="btn btn-outline btn-warning"
+        >
+          {asc ? "Price High to Low" : "Price Low to High"}
+        </button>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
           {services.map((service) => (
             <ServicesCards key={service._id} service={service}></ServicesCards>
